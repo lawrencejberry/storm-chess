f9e0b1bdecf76b4040c4fe0a6e2fef085d43aef2
Lawrence Berry <lawrenceberry@outlook.com>
2020-06-19 12:42:21 +0100

    Highlight possible moves with skeleton logic

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,38 @@
 import React, { useState } from "react";
 import Chessboard from "chessboardjsx";
 
+import getPossibleMoves from "../logic/getPossibleMoves";
+
+const possibleMoveStyle = {
+  background: "radial-gradient(circle, #fffc00 36%, transparent 40%)",
+  borderRadius: "50%",
+};
+
 function Game() {
   const [position, setPosition] = useState("start");
+  const [squareStyles, setSquareStyles] = useState({});
 
-  return <Chessboard position={position} />;
+  const onMouseOverSquare = (square) => {
+    const possibleMoves = getPossibleMoves(square);
+    setSquareStyles(
+      Object.fromEntries(
+        possibleMoves.map((possibleMove) => [possibleMove, possibleMoveStyle])
+      )
+    );
+  };
+
+  const onMouseOutSquare = () => {
+    setSquareStyles({});
+  };
+
+  return (
+    <Chessboard
+      position={position}
+      squareStyles={squareStyles}
+      onMouseOverSquare={onMouseOverSquare}
+      onMouseOutSquare={onMouseOutSquare}
+    />
+  );
 }
 
 export default Game;
