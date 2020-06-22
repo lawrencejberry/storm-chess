50d062e4070815916f14809aa9e19c19f8fa1490
Lawrence Berry <lawrenceberry@outlook.com>
2020-06-22 17:40:20 +0100

    Refactored moves function

--- src/logic/moves.js
+++ src/logic/moves.js
@@ -1,35 +1,37 @@
-function moves(Square){
-    var stormMoves = [];
-//I don't think chess.js has a .turns method (?) so we should just define a variable 'turnCount'.
-//Each time black makes a move, we increment this variable by 1.
-    if(BaseChess.get(Square).type == 'k' && turnCount + 1 == stormTurns[stormLevel]){
- 
-            var starter;
-            if(stormLevel < 2){
-                starter = 16 - (stormLevel*4);
-            }
-            else{
-                starter = 20 - (stormLevel*4)
-            }
-//This will work if we make sure that the 'liveSquares' array is ordered by how soon they get removed- 
-//first 16 squares to get removed are in first 16 positions, etc.
-            for(i = 0; i < BaseChess.moves(Square).length; i++){
-                for(j = starter; j < liveSquares.length; j++){
-                    if(BaseChess.moves(Square)[i] == playableSquares[j]){
-                        stormMoves.push(liveSquares[j])
-                    }
-                }
-            }
-            return stormMoves            
-        }
-    else{
-        for(i = 0; i < BaseChess.moves(Square).length; i++){
-            for(j = 0; j < liveSquares.length; j++){
-                if(BaseChess.moves(Square)[i] == playableSquares[j]){
-                    stormMoves.push(liveSquares[j])
-                }
-            }
-        }
-        return stormMoves
+function moves(Square) {
+  const stormMoves = [];
+  // Turns = length of BaseChess history array
+  let playableSquares = liveSquares;
+  if (
+    Math.floor(BaseChess.history().length / 2) + 1 ===
+    stormTurns[stormLevel]
+  ) {
+    let starter = 16 - stormLevel * 4;
+    if (stormLevel >= 2) {
+      starter = 20 - stormLevel * 4;
     }
+    const nonPlayableSquares = playableSquares.splice(0, starter);
+
+    // Then check if King is in the non playable region
+    if (
+      nonPlayableSquares.some((s) => {
+        const piece = BaseChess.get(s);
+        return piece.type === "k" && piece.color === BaseChess.turn();
+      })
+    ) {
+      if (BaseChess.get(square).type !== "k") {
+        playableSquares = [];
+      }
+    }
+  }
+
+  //This will work if we make sure that the 'liveSquares' array is ordered by how soon they get removed-
+  //first 16 squares to get removed are in first 16 positions, etc.
+
+  for (i = 0; i < BaseChess.moves(Square).length; i++) {
+    if (liveSquares.includes(Basechess.moves(square)[i])) {
+      stormMoves.push(Basechess.moves(square)[i]);
+    }
+  }
+  return stormMoves;
 }
