4e7e456f45aee7a2be7cf272bd8b9fb2a2742fb3
Lawrence Berry <lawrenceberry@outlook.com>
2020-07-03 14:02:47 +0100

    Remove old logic code

--- src/logic/game_over.js
+++
@@ -1,9 +0,0 @@
-function game_over() {
-  return (
-    BaseChess.half_moves >= 100 ||
-    in_checkmate() ||
-    BaseChess.in_stalemate() ||
-    BaseChess.insufficient_material() ||
-    BaseChess.in_threefold_repetition()
-  );
-}
--- src/logic/in_checkmate.js
+++
@@ -1,7 +0,0 @@
-function in_checkmate() {
-  let numLegalMoves = 0;
-  for (let i = 0; i < liveSquares.length(); i++) {
-    numLegalMoves += moves(liveSquares[i]).length();
-  }
-  return numLegalMoves === 0;
-}
